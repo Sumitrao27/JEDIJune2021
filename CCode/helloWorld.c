a62b4366c286288437478fc1692fd0e1fcf0d13a
Sumitrao27 <sumityadav273829@gmail.com>
2021-06-17 11:49:14 +0530

    helloWorld is modified locally

--- CCode/helloWorld.c
+++ CCode/helloWorld.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 
 
-void world(){
- printf("Hello world and the world is awesome");
+void greeting(char name[]);
+
+int main() {
+char name[] = "Alia Bhat"
+greeting(name);
 }
 
-int main(){
-	world();
-	return 0;
-
-}
+void greeting(char name[]) {
+printf("Hey, Where was you?.. %s", name);
+} 
 
