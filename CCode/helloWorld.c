fb98d715d17c37d61b87614d90d8773d87b4c115
Sumitrao27 <40362708+Sumitrao27@users.noreply.github.com>
2021-06-17 11:48:17 +0530

    Update helloWorld.c

--- CCode/helloWorld.c
+++ CCode/helloWorld.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 
 
-void world(){
+void helloWorld(){
  printf("Hello world and the world is awesome");
 }
 
 int main(){
 	world();
+	
+	printf("hello greetings");
 	return 0;
 
 }
